6a5a848b1987defc9413e6eace38a98971d3d4f8
Arthur-Maciel <arthur.maciel.2001@gmail.com>
2020-10-03 13:47:50 -0300

    feat: adding file command

---
+++ bot/commands/JobCommands.js
@@ -0,0 +1,28 @@
+const commandMap = require(".");
+
+const commandFunc = new Map();
+
+commandMap.set("hello", )
+
+module.exports = { 
+    func: function(args) { 
+        return args
+     },
+};
+
+function welcome(author){
+
+}
+
+function lunch(author){
+
+}
+
+function pause(author){
+
+}
+
+function bye(author){
+    
+}
+
---
+++ bot/config/mongo.js
@@ -0,0 +1,3 @@
+module.exports = {
+    port: "mongodb://localhost/myapp",
+}
