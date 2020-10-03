b7d22280b91472cd74b0da73db7658e1860f4619
MarinaFX <marina.moreira@acad.pucrs.br>
2020-10-03 19:19:30 -0300

    feat: added get all repositories from user

--- bot/client/GitHubClients.js
+++ bot/client/GitHubClients.js
@@ -1,1 +1,29 @@
 //client para chamda da api do git
+const fetch = require("node-fetch");
+
+async function getRepositories(username){
+    let repos = [];
+
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}/repos`);
+        const data = await response.json();
+
+        data.forEach(repo => {
+            repos.push({
+                name: repo.name,
+                description: repo.description,
+                htmlUrl: repo.html_url
+            });
+        });
+
+        return repos;
+    } catch (e) {
+        return "CAIU NO CATCH";
+    }
+}
+
+const gitHubClient = {
+    getRepositories: getRepositories,
+}
+
+module.exports = gitHubClient
