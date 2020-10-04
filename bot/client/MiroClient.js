f945452db61777c26941b7acfa00564e6f012fef
Arthur-Maciel <arthur.maciel.2001@gmail.com>
2020-10-04 01:26:20 -0300

    refact: refactoring calls

--- bot/client/MiroClient.js
+++ bot/client/MiroClient.js
@@ -2,13 +2,13 @@ const authentication = require("../config/config.json")
 const fetch = require("node-fetch")
 
 async function getBoard(id){
-    try {
         const response = await fetch(`https://api.miro.com/v1/boards/${id}`, {
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": "Bearer " + authentication.MIRO_TOKEN
             }
         })
+        .catch (e => (e))
 
         const data = await response.json() 
         
@@ -17,13 +17,9 @@ async function getBoard(id){
             description: data.description,
             viewLink: data.viewLink
         }
-    } catch (e) {
-        return "error"
-    }
 }
 
-async function createBoard(){
-    try {
+async function createBoardWithoutTitle(){
         const policy = {
             access: "comment",
             teamAccess: "edit"
@@ -41,21 +37,45 @@ async function createBoard(){
             body: JSON.stringify(request),
             json: true
         })
+        .catch (e => (e))
 
         const data = await response.json() 
-
-        console.log(data)
 
         return {
             viewLink: data.viewLink
         }
-    } catch (e) {
-        return "error"
-    }
+}
+
+async function createBoard(title){
+        const request = {
+            name: title,
+            sharingPolicy: {
+                access: "comment",
+                teamAccess: "edit"
+            }
+        }
+        const response = await fetch(`https://api.miro.com/v1/boards`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + authentication.MIRO_TOKEN
+            },
+            body: JSON.stringify(request),
+            json: true
+        })
+        .catch (e => (e))
+
+        const data = await response.json() 
+
+        return {
+            viewLink: data.viewLink,
+            name: data.name
+        }
 }
 
 const miroClient = {
     get: getBoard,
+    createWithoutTitle: createBoardWithoutTitle,
     create: createBoard,
 }
 
