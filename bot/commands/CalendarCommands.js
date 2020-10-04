3ce36b84c764602b87e36075008f1e3f5dc0d7fd
Henrique R Souza <priv.henrique@gmail.com>
2020-10-03 21:36:45 -0300

    Adicionando create event

---
+++ bot/commands/CalendarCommands.js
@@ -0,0 +1,60 @@
+const convertTime = require('convert-time');
+
+
+function handleCreateEvent(contents) {
+
+    eventObject = {
+        date: "",
+        time: "",
+        name: ""
+    }
+
+    contents.forEach(string => {      
+        if(string.startsWith("name:")) {
+            eventObject.name = string.substring(5);
+        }
+
+        if(string.startsWith("date:")) {
+            eventObject.date = string.substring(5);
+        }
+
+        if(string.startsWith("time:")) {
+            let timePM = string.substring(5);
+            let timeConverted = convertTime(timePM)
+
+            eventObject.time = timeConverted
+        }
+
+    });
+    let dateSplitted = eventObject.date.split('/')
+    let startTimeSplitted = eventObject.time.split(':')
+    
+    let startDate = new Date(dateSplitted[2], dateSplitted[1] - 1, dateSplitted[0], startTimeSplitted[0], startTimeSplitted[1])
+
+    let eventDetails = {
+        description: eventObject.name,
+        dateTime: startDate
+    }
+
+    try {
+        return `Evento ${eventDetails.description} criado! (${eventDetails.dateTime})`
+    } catch (e){
+        return e
+    }
+}
+
+
+// Exports
+module.exports = { 
+    func: function(args) { 
+
+        console.log(args);
+    //Test return
+    if (args[0] === "create") {
+        const event = handleCreateEvent(args);
+        return event;
+    }else{
+        return "Bah viajou";
+    }
+     },
+};
